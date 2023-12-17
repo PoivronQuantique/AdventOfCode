2708baa67386c797e714a4b234a8bc10fdd4b187
Poivron Quantique <110856003+PoivronQuantique@users.noreply.github.com>
2023-12-17 19:52:33 +0100

    .Net Core

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,13 +1,13 @@
-﻿using AdventOfCode.Jours;
+﻿using AdventOfCodeCore.Jours;
 using System;
 
-namespace AdventOfCode
+namespace AdventOfCodeCore
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Jour_abs jour = new Jour_12();
+            Jour_abs jour = new Jour_17();
 
             Console.WriteLine("Part 1 : " + jour.Partie1().ToString().PadLeft(15));
             Console.WriteLine("Part 2 : " + jour.Partie2().ToString().PadLeft(15));
---
+++ AdventOfCodeCore/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCodeCore.Jours;
+using AdventOfCodeCore;
+
+Jour_abs jour = new Jour_17();
+
+Console.WriteLine("Part 1 : " + jour.Partie1().ToString().PadLeft(15));
+Console.WriteLine("Part 2 : " + jour.Partie2().ToString().PadLeft(15));
