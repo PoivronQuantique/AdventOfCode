5bf9eec37145072498f04892f4f59917d334ec23
Poivron Quantique <110856003+PoivronQuantique@users.noreply.github.com>
2024-01-02 21:20:44 +0100

    J21.1 & J22

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,7 +3,7 @@
 using AdventOfCode.Jours;
 using AdventOfCode;
 
-Jour_abs jour = new Jour_20(true);
+Jour_abs jour = new Jour_22();
 
 Console.WriteLine("Part 1 : " + jour.Partie1().ToString().PadLeft(15));
 Console.WriteLine("Part 2 : " + jour.Partie2().ToString().PadLeft(15));
